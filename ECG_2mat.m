9927079699794e852555d1c11d803d1fab25e842
IslandQueen20 <68599288+jordina19@users.noreply.github.com>
2024-04-16 15:19:47 -0400

    Add files via upload
    File that converts the .csv files to .mat files.

---
+++ ECG_2mat.m
@@ -0,0 +1,35 @@
+%Script to process all .csv files in a folder
+
+clear all; % Clear workspace
+clc;       % Clear command window 
+
+%folder = uigetdir; %Get to the directory of the folder containing the file
+
+[originalfileName, folder] = uigetfile("*.csv",'Select CSV Files', 'MultiSelect','on');
+
+if folder == 0
+    error('Error no folder selected');
+end 
+
+fileList = dir(fullfile(folder,'*.csv')); %Access all the files in the folder 
+
+
+for n = 1:length(fileList)
+    %Construct the full file path 
+    fileName = fullfile(fileList(n).folder, fileList(n).name);
+
+    %Read the csv file 
+    T = readtable(fileName);
+
+    %Extract first and seventh column 
+    I  = T(3:end,1);
+    VI = T(3:end,7);
+
+    % Construct the new filename for the MAT file
+    newFileName = fullfile(fileList(n).folder, [fileList(n).name(1:end-4), '_ECG.mat']);
+    
+    % Save the variables to a MAT file
+    save(newFileName, 'I',"VI");
+end
+
+disp(['Sucess!' , num2str(length(fileList)),'files have been converted to .mat']);
