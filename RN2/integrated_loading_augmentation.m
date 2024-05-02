8c1714ba648f0c5a006b85ac36e71a2249565275
wang-jessica <90800349+wang-jessica@users.noreply.github.com>
2024-05-02 02:35:30 -0400

    Update integrated_loading_augmentation.m

--- RN2/integrated_loading_augmentation.m
+++ RN2/integrated_loading_augmentation.m
@@ -1,5 +1,4 @@
 
-
 %% section 1: load all csv files in the selected folder
 clear all; % Clear workspace
 clc;       % Clear command window 
@@ -22,77 +21,108 @@ for n = 1:length(fileList)
     T = readtable(fileName);
     %Extract first and seventh column 
     I  = T(3:end,1);
-    VI = T(3:end,7);
+    %VI = T(3:end,7);
     %Convert tables to two columns 
-    I_VI = [I VI];
+    %I_VI = [I VI];
+    I = [I];
     %Construct the new filename for the MAT file
     newFileName = fullfile(fileList(n).folder, [fileList(n).name(1:end-4), '_ECG.mat']);
     % Save the variables to a MAT file
-    save(newFileName, 'I_VI');
+    % save(newFileName, 'I_VI');
+    save(newFileName, 'I');
 end
 disp(['Sucess!' , num2str(length(fileList)),'files have been converted to .mat']);
 
 %% Section 2: for each csv file, perform 3 different augmentations
-% store the results in a new 3-column csv file 
-% column 1: 'Augmentation #1 - Gaussian Noise + Baseline Wander'
-% column 2: 'Augmentation #2 - Butterworth Filtering + Line Noise'
-% column 3: 'Augmentation #3 - Dropout + Baseline Wander'
+% store the results in a new 4-column csv file 
+% column 1: original signal
+% column 2: augmentation combo 1
+% column 3: augmentation combo 2
+% column 4: augmentation combo 3
 
 for n = 1:length(csvList)
     disp(int2str(n))
     ECG = readmatrix(csvList{n});
     ECG(1:2,:) = [];
     lead_1 = ECG(:,1);
-    for n=1:3
-    if n==1
-        noisy_sig = gaussian_augmentation(lead_1);
-        output(:,1) = baseline_wander_augmentation(noisy_sig);
-    elseif n==2
-        butterworth_sig = butterworth_filter_augmentation(lead_1);
-        output(:,2) = line_noise_augmentation(butterworth_sig);
-    elseif n==3
-        dropout_sig = dropout_augmentation(lead_1);
-        output(:,3) = baseline_wander_augmentation(dropout_sig);
+    output(:,1) = lead_1;
+    for k=1:3
+        aug = randi(6);
+        augmented = lead_1;
+        if aug==1
+            augmented = gaussian_augmentation(augmented);
+            augmented = baseline_wander_augmentation(augmented);
+        elseif aug==2
+            augmented = gaussian_augmentation(augmented);
+            augmented = butterworth_filter_augmentation(augmented);
+        elseif aug==3
+            augmented = gaussian_augmentation(augmented);
+            augmented = dropout_augmentation(augmented);
+        elseif aug==4
+            augmented = gaussian_augmentation(augmented);
+            augmented = line_noise_augmentation(augmented);
+        elseif aug==5
+            augmented = baseline_wander_augmentation(augmented);
+            augmented = butterworth_filter_augmentation(augmented);
+        elseif aug==6
+            augmented = baseline_wander_augmentation(augmented);
+            augmented = dropout_augmentation(augmented);
+        elseif aug==7
+            augmented = baseline_wander_augmentation(augmented);
+            augmented = line_noise_augmentation(augmented);
+        elseif aug==8
+            augmented = butterworth_filter_augmentation(augmented);
+            augmented = dropout_augmentation(augmented);
+        elseif aug==9
+            augmented = butterworth_filter_augmentation(augmented);
+            augmented = line_noise_augmentation(augmented);
+        elseif aug==10
+            augmented = dropout_augmentation(augmented);
+            augmented = line_noise_augmentation(augmented);
+        end
+       output(:,k+1) = augmented;
     end
+    
     % Save "output" array as a MAT file
     newFileName = fullfile(fileList(n).folder, [fileList(n).name(1:end-4), '_augmented.mat']);
     save(newFileName, 'output');
     % save "output" array as a csv file
-    writematrix(output,[newFileName,'.csv'])
-    end
+    writematrix(output,[newFileName,'.csv']);
 end
 
 
 %%
-Fs=500;
-t=0:1/Fs:10;
-t=t(1:end-1);
+%Fs=500;
+%t=0:1/Fs:10;
+%t=t(1:end-1);
+%t = t(1:length(lead_1));
 
+figure
 subplot(4,1,1)
-plot(t,lead_1)
+plot(normal)
 title('Raw WPW ECG - Lead I')
 xlabel('Time (s)')
 ylabel('Amplitude')
 
 subplot(4,1,2)
-plot(t,output(:,1))
-title('Augmentation #1 - Gaussian Noise + Baseline Wander')
+plot(aug1)
+title('Augmentation #1')
 xlabel('Time (s)')
 ylabel('Amplitude')
 
 subplot(4,1,3)
-plot(t,output(:,2))
-title('Augmentation #2 - Butterworth Filtering + Line Noise')
+plot(aug2)
+title('Augmentation #2')
 xlabel('Time (s)')
 ylabel('Amplitude')
 
 subplot(4,1,4)
-plot(t,output(:,3))
-title('Augmentation #3 - Dropout + Baseline Wander')
+plot(aug3)
+title('Augmentation #3')
 xlabel('Time (s)')
 ylabel('Amplitude')
 
-
+%%
 
 function [noisy_sig] = gaussian_augmentation(input_lead)
 %Gaussian Data Augmentation sub-function
@@ -105,7 +135,9 @@ end
 function [dropout_sig] = dropout_augmentation(input_sig)
 %dropout augmentation sub-function
 %randomly chooses 5% (from literature) of data points to drop-out
-idx = randperm(numel(input_sig),round(numel(input_sig))*5/100) ; 
+%disp('numel(input_sig)')
+
+idx = randperm(numel(input_sig),round(numel(input_sig)*5/100)) ; 
 dropout_sig = input_sig;
 dropout_sig(idx)=0;
 end
@@ -123,10 +155,13 @@ for k=1:3
 end
 Fs=500;
 t=0:1/Fs:10;
-t=t(1:end-1);
+%t=t(1:end-1);
+t = t(1:length(input_sig));
 rand_sin = (maxi*rand_sin_scale(1)*sin(2*pi*rand_sin_freq(1)*t + rand_phase(1)) + ...
     maxi*rand_sin_scale(2)*sin(2*pi*rand_sin_freq(2)*t + rand_phase(2)) + ...
     maxi*rand_sin_scale(3)*sin(2*pi*rand_sin_freq(3)*t + rand_phase(3)))';
+%disp(size(input_sig))
+%disp(size(rand_sin))
 wander_sig =  + input_sig + rand_sin;
 end
 
@@ -153,7 +188,8 @@ rand_phase = 0 + 2*pi*rand(1);
 amp_sin = max(input_sig)*rand_amp;
 Fs=500;
 t=0:1/Fs:10;
-t=t(1:end-1);
+%t=t(1:end-1);
+t = t(1:length(input_sig));
 linenoise_sig = input_sig + (amp_sin*sin(2*pi*freq_ln*t + rand_phase))';
 end
 
